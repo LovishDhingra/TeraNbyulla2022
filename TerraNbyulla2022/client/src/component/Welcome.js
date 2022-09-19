1d3f29830ea0bbc8eae580216f7b26e01bb3d4bb
LovishDhingra <lovishdhingralove@gmail.com>
2022-09-19 14:28:28 +0530

    fourth commit

---
+++ TerraNbyulla2022/client/src/component/Welcome.js
@@ -0,0 +1,38 @@
+import { Grid, Typography } from "@material-ui/core";
+
+const Welcome = (props) => {
+  return (
+    <Grid
+      container
+      item
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ padding: "30px", minHeight: "93vh" , background:"brown" }}
+    >
+      <Grid item>
+        <Typography style={{color:"lightpink"}} variant="h2">Welcome to  Nbyula </Typography>
+        <Typography style={{color:"grey"}} variant="h5">Find relevent jobs for you {<b>{"-->"}</b>} </Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export const ErrorPage = (props) => {
+  return (
+    <Grid
+      container
+      item
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ padding: "30px", minHeight: "93vh" }}
+    >
+      <Grid item>
+        <Typography variant="h2">Error 404</Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Welcome;
